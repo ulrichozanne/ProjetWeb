93697105626f570bea6c5e8eef669c1a145a41c8
ulrichozanne <51369124+ulrichozanne@users.noreply.github.com>
2019-12-09 09:58:45 +0100

    suppression_connexion.php
    milou you suck

--- index.php
+++ index.php
@@ -1,23 +1,3 @@
-<?php 
-try
-{
-	// On se connecte à MySQL
-	$bdd = new PDO('mysql:host=localhost;dbname=cashcash;charset=utf8', 'root', '');
-}
-catch(Exception $e)
-{
-	// En cas d'erreur, on affiche un message et on arrête tout
-        die('Erreur : '.$e->getMessage());
-}
-/*if(isset($_POST['connexion']))
-{
-	$login = $_POST['login'];
-	$mdp = $_POST['mdp'];
-}
-*/
- ?>
-
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
